77b638a1acbcb93dd160d7ab4d01eed1c9110206
Duarda <111713157+du4ards09@users.noreply.github.com>
2022-09-16 11:02:40 -0300

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,40 +1,41 @@
---DROP DATABASE bdEstoque
+DROP DATABASE bdEstoque
 
-CREATE DATABASE bdEstoque2 
-GO
+CREATE DATABASE bdEstoque
 
-USE bdEstoque2
+USE bdEstoque
 
 
 CREATE TABLE tbFabricante(
 	idFabricante INT PRIMARY KEY IDENTITY(1,1)
 	, nomeFabricante VARCHAR(50)
 )
+CREATE TABLE tbFornecedor(
+	idFornecedor INT PRIMARY KEY IDENTITY (1,1)
+	,nomeFornecedor VARCHAR(30) NOT NULL
+	,contatoFornecedor VARCHAR(20) NOT NULL
+)
+
 CREATE TABLE tbProduto(
 	idProduto INT PRIMARY KEY IDENTITY(1,1)
 	,descricaoProduto VARCHAR (40) NOT NULL
 	,valorProduto SMALLMONEY NOT NULL
 	,quantidadeProduto INT NOT NULL
 	,idFabricante INT FOREIGN KEY REFERENCES tbFabricante(idFabricante)
+	, idFornecedor INT FOREIGN KEY REFERENCES tbFornecedor(idFornecedor)
 )
-CREATE TABLE tbFornecedor(
-	idFornecedor INT PRIMARY KEY IDENTITY (1,1)
-	,nomeFornecedor VARCHAR(30) NOT NULL
-	,contatoFornecedor VARCHAR(20) NOT NULL
+CREATE TABLE tbCliente(
+	idCliente INT PRIMARY KEY IDENTITY (1,1)
+	,nomeCliente VARCHAR(50)
+	,cpfCliente VARCHAR(12) NOT NULL
+	,emailCliente VARCHAR(40) NOT NULL
+	,sexoCliente CHAR(1) NOT NULL
+	,dataNascCliente SMALLDATETIME NOT NULL
 )
 CREATE TABLE tbVenda(
 	idVenda INT PRIMARY KEY IDENTITY(1,1)
 	,dataVenda SMALLDATETIME NOT NULL
-	,valortotaldaVenda SMALLMONEY NOT NULL
+	,valorTotalVenda SMALLMONEY NOT NULL
 	,idCliente INT FOREIGN KEY REFERENCES tbCliente(idCliente)
-)
-CREATE TABLE tbCliente(
-	idCliente INT PRIMARY KEY REFERENCES (1,1)
-	,nomeCliente VARCHAR(50)
-	,cpfCliente CHAR(11) NOT NULL
-	,emailCliente VARCHAR(40) NOT NULL
-	,sexoCliente CHAR(1) NOT NULL
-	,dataNascCliente SMALLDATETIME NOT NULL
 )
 CREATE TABLE tbItensVenda(
 	idItensVenda INT PRIMARY KEY IDENTITY (1,1)
