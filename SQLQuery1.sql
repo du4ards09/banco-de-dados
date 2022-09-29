5ae0adc12a86f62bab614cd60ec17ce38c51a61c
Duarda <111713157+du4ards09@users.noreply.github.com>
2022-09-28 23:10:00 -0300

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,46 +1,47 @@
-DROP DATABASE bdEstoque
-
-CREATE DATABASE bdEstoque
-
-USE bdEstoque
+CREATE DATABASE BdEscolaIdiomas
+ 
+ USE bdEscolaIdiomas
 
 
-CREATE TABLE tbFabricante(
-	idFabricante INT PRIMARY KEY IDENTITY(1,1)
-	, nomeFabricante VARCHAR(50)
-)
-CREATE TABLE tbFornecedor(
-	idFornecedor INT PRIMARY KEY IDENTITY (1,1)
-	,nomeFornecedor VARCHAR(30) NOT NULL
-	,contatoFornecedor VARCHAR(20) NOT NULL
+ CREATE TABLE tbCurso(
+	codCurso INT PRIMARY KEY (1,1)
+	, nomeCurso VARCHAR(10) NOT NULL
 )
 
-CREATE TABLE tbProduto(
-	idProduto INT PRIMARY KEY IDENTITY(1,1)
-	,descricaoProduto VARCHAR (40) NOT NULL
-	,valorProduto SMALLMONEY NOT NULL
-	,quantidadeProduto INT NOT NULL
-	,idFabricante INT FOREIGN KEY REFERENCES tbFabricante(idFabricante)
-	, idFornecedor INT FOREIGN KEY REFERENCES tbFornecedor(idFornecedor)
+CREATE TABLE tbPeriodo(
+	codPeriodo INT PRIMARY KEY (1,1)
+	, descPeriodo VARCHAR(9) NOT NULL
 )
-CREATE TABLE tbCliente(
-	idCliente INT PRIMARY KEY IDENTITY (1,1)
-	,nomeCliente VARCHAR(50)
-	,cpfCliente VARCHAR(12) NOT NULL
-	,emailCliente VARCHAR(40) NOT NULL
-	,sexoCliente CHAR(1) NOT NULL
-	,dataNascCliente SMALLDATETIME NOT NULL
+
+CREATE TABLE tbDiaSemana(
+	codDiaSemana INT PRIMARY KEY (1,1)
+	, diaSemana VARCHAR(13) NOT NULL
 )
-CREATE TABLE tbVenda(
-	idVenda INT PRIMARY KEY IDENTITY(1,1)
-	,dataVenda SMALLDATETIME NOT NULL
-	,valorTotalVenda SMALLMONEY NOT NULL
-	,idCliente INT FOREIGN KEY REFERENCES tbCliente(idCliente)
+CREATE TABLE tbNivel(
+	codNivel INT PRIMARY KEY (1,1)
+	, descNivel VARCHAR(14) NOT NULL
 )
-CREATE TABLE tbItensVenda(
-	idItensVenda INT PRIMARY KEY IDENTITY (1,1)
-	,quantidadeItensVenda INT NOT NULL
-	,subtotalItensVendas SMALLMONEY NOT NULL
-	,idVenda INT FOREIGN KEY REFERENCES tbVenda(idVenda)
-	,idProduto INT FOREIGN KEY REFERENCES tbProduto(idProduto)
+CREATE TABLE tbAluno(
+	codAluno INT PRIMARY KEY (1,1)
+	, nomeAluno VARCHAR(35) NOT NULL
+	, rgAluno VARCHAR(12) NOT NULL
+	, enderecoAluno VARCHAR(30) NOT NULL
+	, numAluno VARCHAR(4) NOT NULL
+	, compAluno VARCHAR(7) NOT NULL
+	, bairroAluno VARCHAR(20) NOT NULL
+	, cepAluno VARCHAR (8) NOT NULL
+	, cidadeAluno VARCHAR(20) NOT NULL
+)
+CREATE TABLE tbTurma(
+	codTurma INT PRIMARY KEY (1,1)
+	, horarioTurma TIME NOT NULL
+	codCurso INT FOREIGN KEY REFERENCES tbCurso(codCurso)
+	codPeriodo INT FOREIGN KEY REFERENCES tbPeriodo(codPeriodo)
+	codNivel INT FOREIGN KEY REFERENCES tbNivel(codNivel)
+	codDiaSemana INT FOREIGN KEY REFERENCES tbDiaSemana(codDiaSemana)
+)
+CREATE TABLE tbMatricula(
+	codMatricula INT PRIMARY KEY (1,1)
+	, dataMatricula SMALLDATETIME NOT NULL
+	cod
 )
