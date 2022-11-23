b54d6acc0dbdc57cf91ee095bda6cb9b115796fb
Duarda <111713157+du4ards09@users.noreply.github.com>
2022-11-23 20:07:45 -0300

    Create insertBdLojaRoupa.sql

---
+++ BdLojaRoupas/insertBdLojaRoupa.sql
@@ -0,0 +1,25 @@
+USE bdLojaRoupas2
+
+SELECT*FROM tbFabricante
+SELECT*FROM tbProduto
+SELECT*FROM tbItensVenda
+
+UPDATE tbFabricante
+SET nomeFabricante ="Turma da Malha"
+WHERE codFabricante =2
+
+UPDATE tbProduto
+SET valorProduto = valorProduto*0.8
+WHERE codProduto = 2
+
+UPDATE tbProduto
+SET estoqueProduto = 10
+WHERE codProduto = 3
+
+DELETE FROM tbVenda
+WHERE codVenda = 2
+
+DELETE FROM tbItensVenda
+SET 
+WHERE 
+--N�o removemos o Fabricante ainda !!
